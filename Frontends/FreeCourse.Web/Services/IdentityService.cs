1a3490aa4b1dbd61db0cef483287b9e6ea33023f
Melike Erbaş <melike@elephantapps.co>
2022-05-17 12:56:25 +0300

    inital

--- Frontends/FreeCourse.Web/Services/IdentityService.cs
+++ Frontends/FreeCourse.Web/Services/IdentityService.cs
@@ -2,9 +2,17 @@
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Interface;
 using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
@@ -35,10 +43,68 @@ namespace FreeCourse.Web.Services
             throw new System.NotImplementedException();
         }
 
-        public Task<Response<bool>> SıgnIn(SıgnInInput model)
+        public async Task<Response<bool>> SıgnIn(SıgnInInput model)
         {
             //Token endpoint'e git.
-            throw new System.NotImplementedException();
+            var disco = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.BaseUri,
+                Policy = new DiscoveryPolicy { RequireHttps=false}
+            });
+
+            if (disco.IsError)
+            {
+                throw disco.Exception;
+            }
+
+            var passwordTokenRequest = new PasswordTokenRequest
+            {
+                ClientId = _clientSettings.WebClientForUser.ClientId,
+                ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+                UserName = model.Email,
+                Password = model.Password,
+                Address = disco.TokenEndpoint
+            };
+
+            var token = await _httpClient.RequestPasswordTokenAsync(passwordTokenRequest);
+            if (token.IsError)
+            {
+                var responseContent = await token.HttpResponse.Content.ReadAsStringAsync();
+                var errorDto = JsonSerializer.Deserialize<ErrorDto>(responseContent,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive=false});
+                return Response<bool>.Fail(errorDto.Errors,400);
+            }
+
+            //claim nesnelerine ulasabilmek icin userinfo endpoint'ine token yollayarak alabiliriz.
+            var userInfoRequest = new UserInfoRequest
+            {
+                Token = token.AccessToken,
+                Address = disco.UserInfoEndpoint
+            };
+
+            var userInfo = await _httpClient.GetUserInfoAsync(userInfoRequest);
+            if (userInfo.IsError)
+            {
+                throw userInfo.Exception;
+            }
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(userInfo.Claims,CookieAuthenticationDefaults.AuthenticationScheme,"name","role");
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            var authenticationProperties = new AuthenticationProperties();
+            authenticationProperties.StoreTokens(new List<AuthenticationToken>()
+            {
+                new AuthenticationToken{ Name = OpenIdConnectParameterNames.AccessToken,Value = token.AccessToken},
+                new AuthenticationToken{ Name = OpenIdConnectParameterNames.RefreshToken,Value = token.RefreshToken},
+                new AuthenticationToken{ Name = OpenIdConnectParameterNames.ExpiresIn,Value = 
+                DateTime.Now.AddSeconds(token.ExpiresIn).ToString("o",CultureInfo.InvariantCulture)},
+
+            });
+
+            authenticationProperties.IsPersistent = model.IsRemember;
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                claimsPrincipal,authenticationProperties);
+            return Response<bool>.Success(200);
         }
     }
 }
